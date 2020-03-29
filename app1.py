4f8699e60089377909dd4940d7bd950253637eec
Ash_win <62272001+ash-win-cs@users.noreply.github.com>
2020-03-29 19:16:35 +0530

    new file

---
+++ app1.py
@@ -0,0 +1,29 @@
+#Entry widgets
+from tkinter import *
+
+root = Tk()
+root.title("Entry widgets")
+root.geometry("400x400")
+
+
+#create click function
+def clicked():
+    my_label2 = Label(root, text= "Hello " + e.get())
+    my_label2.pack()
+    
+
+#Create label widget
+my_label = Label(root, text="Entry Widgets", fg="white", bg="black", font=("algerian", 22)).pack()
+
+#Create label widget
+my_label1 = Label(root, text="Enter your name: ").pack()
+
+#Create entry widget
+e = Entry(root, font=("Helvetica",10))
+e.pack(pady=10)
+
+#create Buttons
+my_button = Button(root, text="Button", command=clicked).pack()
+
+
+root.mainloop()
