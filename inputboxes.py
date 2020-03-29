32d9bdda36a16a087a30cbc2299f69fdbde3a198
Ash_win <62272001+ash-win-cs@users.noreply.github.com>
2020-03-29 18:43:15 +0530

    now you can take values from entr7y box

--- inputboxes.py
+++ inputboxes.py
@@ -8,17 +8,22 @@ root.geometry("400x400")
 
 #create click function
 def clicked():
-    my_label2 = Label(root, text="Value Recorded").pack()
+    my_label2 = Label(root, text=e.get())
+    my_label2.pack()
     
 
 #Create label widget
 my_label = Label(root, text="Entry Widgets", fg="white", bg="black", font=("algerian", 22)).pack()
 
+#Create label widget
+my_label1 = Label(root, text="Enter your name: ").pack()
+
 #Create entry widget
-e = Entry(root)
+e = Entry(root, font=("Helvetica",10))
 e.pack(pady=10)
 
 #create Buttons
 my_button = Button(root, text="Button", command=clicked).pack()
+
 
 root.mainloop()
