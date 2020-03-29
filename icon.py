b19282651574edad56cdc80dad7f1e9aaca4903f
Ash_win <62272001+ash-win-cs@users.noreply.github.com>
2020-03-29 19:06:27 +0530

    added icons to window

---
+++ icon.py
@@ -0,0 +1,30 @@
+#this program is to add to the window from default
+#Entry widgets
+from tkinter import *
+
+root = Tk()
+root.title("Entry widgets")
+root.geometry("400x400")
+root.iconbitmap("C:/Users/acer/Documents/gui/enigma.ico")
+
+#create click function
+def clicked():
+    my_label2 = Label(root, text= "Hello " + e.get())
+    my_label2.pack()
+    
+
+#Create label widget
+my_label = Label(root, text="Entry Widgets", fg="white", bg="black", font=("algerian", 22)).pack()
+
+#Create label widget
+my_label1 = Label(root, text="Enter your name: ").pack()
+
+#Create entry widget
+e = Entry(root, font=("Helvetica",10))
+e.pack(pady=10)
+
+#create Buttons
+my_button = Button(root, text="Button", command=clicked).pack()
+
+
+root.mainloop()
